cab2135674e40c124c1eea8224d12cea5d8e55f2
Kummey <alexander@thomas-kummerer.de>
2021-04-14 16:11:44 +0200

    created my location

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyLocationComponent } from './my-location/my-location.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MyLocationComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
