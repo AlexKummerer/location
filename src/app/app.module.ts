8d6c0dab3c3fc2cb0b39baa4e1df8d4459f12d3b
Kummey <alexander@thomas-kummerer.de>
2021-04-15 13:10:05 +0200

    search address in google API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyLocationComponent } from './my-location/my-location.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { DialogAddLocationComponent } from './dialog-add-location/dialog-add-location.component';
-import {MatDialogModule} from '@angular/material/dialog';
-import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MyLocationComponent,
-    DialogAddLocationComponent
-  ],
+  declarations: [AppComponent, MyLocationComponent, DialogAddLocationComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -26,10 +25,12 @@ import {MatButtonModule} from '@angular/material/button';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    GooglePlaceModule,
+    MatAutocompleteModule,
 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/dialog-add-location/dialog-add-location.component.ts
+++ src/app/dialog-add-location/dialog-add-location.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import {startWith, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-dialog-add-location',
@@ -7,12 +10,53 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./dialog-add-location.component.scss'],
 })
 export class DialogAddLocationComponent implements OnInit {
+  streets: any;
   constructor(public dialogRef: MatDialogRef<DialogAddLocationComponent>) {}
-
+  // API_KEY  = ''https://maps.googleapis.com/maps/api/geocode/json?address=' +
+  // this.address +
+  // '&key=' +
+  // apiKey;  '
+  control = new FormControl();
+  autocompleteInput: string;
   addressSelected = false;
+  address: string = '';
+  addreses = []
+  filteredAddresses: Observable<string[]>;
+  ngOnInit(): void {
+    this.filteredAddresses = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    )
+  }
+  private _filter(value: any) {
+    const filterValue = this._normalizeValue(value);
+    return this.addreses.filter(address => this._normalizeValue(address).includes(filterValue));
+  }
 
-  ngOnInit(): void {}
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
   changeLocation() {
     this.addressSelected = !this.addressSelected;
   }
+
+  async getAddress(data) {
+    this.address = data;
+    let apiKey = 'AIzaSyDyAbpvB9qmUgN6gfNf0bB_uZ5YSJueRIk';
+    const url =
+      'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+      this.address +
+      '&key=' +
+      apiKey;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        this.addreses = json.results;
+        console.log(this.addreses);
+      });
+  }
 }
+
+
